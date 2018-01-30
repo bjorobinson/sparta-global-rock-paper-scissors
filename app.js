686bdfed83012c0a89a1b04044a97635d4bf7191
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-01-30 17:35:31 +0000

    Added AI move generation

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+//Gets player input as string
 var playerinput = prompt("Welcome. Enter your move: rock, paper, scissors")
+//Converts player input into a number between 0 and 2 with the scheme:
 //rock = 0
 //paper = 1
 //scissors = 2
@@ -15,3 +17,7 @@ switch (playerinput) {
   default:
     throw 'invalidInputError';
 }
+
+//Generates number between 0 and 2 for computer move
+console.log(Math.floor(Math.random()*3));
+var computer = Math.floor(Math.random()*3);
