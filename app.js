09b46d78cde0e00a8e172312a5dbb7fc7e422580
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-01-30 17:48:02 +0000

    Implemented basic single game

--- app.js
+++ app.js
@@ -21,3 +21,16 @@ switch (playerinput) {
 //Generates number between 0 and 2 for computer move
 console.log(Math.floor(Math.random()*3));
 var computer = Math.floor(Math.random()*3);
+
+
+//Game itself:
+//variable to check if player wins (1). Default to compuer winning (0). Draw is when no one wins (2).
+var win = 0;
+//Win state 1: player number > computer number. Ie, player is paper, computer is rock; player is scissors, computer is paper
+if (player > computer) {
+  win = 1;
+} else if (player==0 && computer==2){ //Win state 2: player is 0, and computer is 2. Ie, player is rock, computer is scissors
+  win = 1;
+} else if (player==computer){ //Draw state: player and computer choose the same thing
+  win = 2;
+}
